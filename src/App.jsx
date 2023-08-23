ce315ea3d962cf00302d6e60c9434b951bd54d0f
emaulandi <e.maulandi@gmail.com>
2023-08-23 16:42:56 +0200

    Set up podcasts, categories and episodes list, filter on category

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,51 @@
 import { useState } from 'react'
 import './App.css'
 
+import data from './assets/podcasts_episodes_manual_update.csv'
+import { podcastsConfig } from './config'
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const displayData = data
+    .filter(({ filterOut }) => parseInt(filterOut) !== 1)
+    .sort((a, b) => a.date < b.date)
+
+  const podcasts = [... new Set(displayData.map(({ podcast }) => podcast))];
+  const initialCategories = [... new Set(displayData.map(({ category }) => category.split(',')).flat())];
+
+  const [categories, setCategories] = useState(initialCategories);
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <h1>Explorez les podcasts</h1>
+      <h2>Podcasts</h2>
+      <div className="podcasts">
+        {podcasts.map(p => (
+          <div className="podcast" key={p} style={{ backgroundColor: podcastsConfig[p].color }}>{p}</div>
+        ))}
+      </div>
+      <h2>Thèmes</h2>
+      <div className="categories">
+        {initialCategories.map(c => (
+          <div className="category" key={c} style={{ fontWeight: categories.includes(c) ? 900 : 300}}>
+            <button onClick={() => setCategories([c])}>
+              {c}
+            </button>
+          </div>
+        ))}
+      </div>
+      <button style={{ fontStyle: 'italic', fontWeight: 300 }} onClick={() => setCategories(initialCategories)}>
+        Sélectionnez toutes les catégories
+      </button>
+      <h2>Episodes</h2>
+      <div className="episodes">
+        {displayData
+          .filter(({ category }) => categories.some(cat => category.split(',').includes(cat)))
+          .map(({ name, podcast }) => (
+          <div className="episode" key={name} style={{ backgroundColor: podcastsConfig[podcast].color }}>
+            <span>{name}</span>
+          </div>
+        ))}
       </div>
     </>
   )
---
+++ src/config.js
@@ -0,0 +1,14 @@
+export const podcastsConfig = {
+    'Ozé. Comprendre. S\'inspirer. S\'engager': {
+        color: '#FCCA4D'
+    },
+    'Plan(s) B': {
+        color: '#BBD8B3'
+    },
+    'Présages': {
+        color: '#C09CC9'
+    },
+    'Sismique': {
+        color: '#E08888'
+    },
+}
