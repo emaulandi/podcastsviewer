c7a85f8fa87dc7b0cdb0e5875d8609a210d1caa0
emaulandi <e.maulandi@gmail.com>
2023-08-24 19:24:09 +0200

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import dsv from '@rollup/plugin-dsv'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'podcastsviewer',
+  base: '/podcastsviewer/',
   plugins: [
     react(),
     dsv()
